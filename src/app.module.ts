f83596ea55ab8bb6b88e69572e9b49689bcbe2b0
Cesar Salazar <bitemecesar@gmail.com>
2022-07-16 21:17:39 -0400

    Change remote DB url

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,18 +17,13 @@ import { CategoriesModule } from './categories/categories.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: process.env.POSTGRES_HOST,
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      url: 'postgres://wurhfjxedwysmc:9a7a3094614e7c98de7479cc98f62b1d437d1720978796a5fedf0a8bf1fb8c8c@ec2-3-217-14-181.compute-1.amazonaws.com:5432/d2etbstc2806m7',
       // entities: ['dist/**/*.entity{.ts,.js}'],
       autoLoadEntities: true,
       synchronize: true,
+      logging: true,
       retryDelay: 3000,
       retryAttempts: 10,
-      ssl: true,
       extra: {
         ssl: {
           rejectUnauthorized: false,

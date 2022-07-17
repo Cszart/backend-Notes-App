bbab5ae774ff163818abfd89e639d587a8e612dc
Cesar Salazar <bitemecesar@gmail.com>
2022-07-17 10:27:27 -0400

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const cors = require('cors');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  app.use(cors());
 
   await app.listen(process.env.PORT || 5000);
 }
